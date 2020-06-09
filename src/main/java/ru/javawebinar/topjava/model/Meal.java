eb9f018a60be60e8d593f39142d0510cebbcdb82
Julia Goryacheva <juligo04@gmail.com>
2020-06-10 01:46:56 +0300

    HW1. update model

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -3,18 +3,39 @@ package ru.javawebinar.topjava.model;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.Objects;
+import java.util.UUID;
 
 public class Meal {
-    private final LocalDateTime dateTime;
+    public static final Meal EMPTY = new Meal();
 
-    private final String description;
+    private String uuid;
 
-    private final int calories;
+    private LocalDateTime dateTime;
+
+    private String description;
+
+    private int calories;
+
+    public Meal() {
+    }
 
     public Meal(LocalDateTime dateTime, String description, int calories) {
+        this(UUID.randomUUID().toString(), dateTime, description, calories);
+    }
+
+    public Meal(String uuid, LocalDateTime dateTime, String description, int calories) {
+        Objects.requireNonNull(uuid, "uuid must not be null");
+        Objects.requireNonNull(dateTime, "date must not be null");
+        Objects.requireNonNull(description, "description must not be null");
+        this.uuid = uuid;
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
+    }
+
+    public String getUuid() {
+        return uuid;
     }
 
     public LocalDateTime getDateTime() {
@@ -35,5 +56,38 @@ public class Meal {
 
     public LocalTime getTime() {
         return dateTime.toLocalTime();
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Meal meal = (Meal) o;
+        return Objects.equals(uuid, meal.uuid) &&
+                Objects.equals(dateTime, meal.dateTime) &&
+                Objects.equals(description, meal.description) &&
+                Objects.equals(calories, meal.calories);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(uuid, dateTime, description, calories);
+    }
+
+    @Override
+    public String toString() {
+        return dateTime + " " + description + " " + calories;
     }
 }
--- src/main/java/ru/javawebinar/topjava/model/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/model/MealTo.java
@@ -1,8 +1,12 @@
 package ru.javawebinar.topjava.model;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class MealTo {
+
+    private final String uuid;
+
     private final LocalDateTime dateTime;
 
     private final String description;
@@ -11,20 +15,60 @@ public class MealTo {
 
     private final boolean excess;
 
-    public MealTo(LocalDateTime dateTime, String description, int calories, boolean excess) {
+    public MealTo(String uuid, LocalDateTime dateTime, String description, int calories, boolean excess) {
+        Objects.requireNonNull(uuid, "uuid must not be null");
+        Objects.requireNonNull(dateTime, "date must not be null");
+        Objects.requireNonNull(description, "description must not be null");
+        this.uuid = uuid;
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
         this.excess = excess;
     }
 
+    public MealTo(Meal meal, boolean excess) {
+        this(meal.getUuid(), meal.getDateTime(), meal.getDescription(), meal.getCalories(), excess);
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public boolean isExcess() {
+        return excess;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MealTo mealTo = (MealTo) o;
+        return Objects.equals(uuid, mealTo.uuid) &&
+                Objects.equals(dateTime, mealTo.dateTime) &&
+                Objects.equals(description, mealTo.description) &&
+                Objects.equals(calories, mealTo.calories) &&
+                Objects.equals(excess, mealTo.excess);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(uuid, dateTime, description, calories, excess);
+    }
+
     @Override
     public String toString() {
-        return "MealTo{" +
-                "dateTime=" + dateTime +
-                ", description='" + description + '\'' +
-                ", calories=" + calories +
-                ", excess=" + excess +
-                '}';
+        return dateTime + " " + description + " " + calories + " " + excess;
     }
 }
